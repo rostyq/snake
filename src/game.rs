61ad5eb1cbf4d08b22d8cd87ddabddcae9dc3a6b
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2022-05-23 22:48:48 +0300

    # Placing the Fruit

--- src/game.rs
+++ src/game.rs
@@ -1,29 +1,57 @@
-use crate::snake::Snake;
-use crate::wasm4::{BUTTON_DOWN, BUTTON_LEFT, BUTTON_RIGHT, BUTTON_UP, GAMEPAD1};
+use crate::palette::set_draw_color;
+use crate::snake::{Point, Snake};
+use crate::wasm4::{BUTTON_DOWN, BUTTON_LEFT, BUTTON_RIGHT, BUTTON_UP, GAMEPAD1, blit, BLIT_2BPP};
+use fastrand::Rng;
+
+const FRUIT_WIDTH: u32 = 8;
+const FRUIT_HEIGHT: u32 = 8;
+const FRUIT_FLAGS: u32 = BLIT_2BPP;
+const FRUIT_SPRITE: [u8; 16] = [
+    0x00, 0xa0, 0x02, 0x00, 0x0e, 0xf0, 0x36, 0x5c, 0xd6, 0x57, 0xd5, 0x57, 0x35, 0x5c, 0x0f, 0xf0,
+];
 
 pub struct Game {
+    rng: Rng,
     snake: Snake,
     frame_count: u64,
     prev_gamepad: u8,
+    fruit: Point,
 }
 
 impl Game {
     pub fn new() -> Self {
+        let rng = Rng::with_seed(42);
         Self {
             snake: Snake::new(),
             frame_count: 0,
             prev_gamepad: 0,
+            fruit: Point {
+                x: rng.i32(0..20),
+                y: rng.i32(0..20),
+            },
+            rng,
         }
+    }
+
+    fn draw_fruit(&self) {
+        set_draw_color(0x4320);
+        blit(&FRUIT_SPRITE, self.fruit.x * 8, self.fruit.x * 8, FRUIT_WIDTH, FRUIT_HEIGHT, FRUIT_FLAGS);
+    }
+
+    fn render(&self) {
+        self.snake.draw();
+        self.draw_fruit();
     }
 
     pub fn update(&mut self) {
         self.frame_count += 1;
         self.input();
+
         if self.frame_count % 15 == 0 {
             self.snake.update();
         }
 
-        self.snake.draw();
+        self.render();
     }
 
     fn input(&mut self) {
